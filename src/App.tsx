d24c9f846a1dd3e6bbe22210f00e0175f0d48ef1
silver <ekdldkaa@naver.com>
2023-11-18 14:51:36 +0900

    [feat]PageRouting

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import Landing from './pages/landing';
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
-  return <div className="App">HelloWorld</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Landing />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/pages/landing.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const Landing = () => {
+  return (
+    <div>
+      <h2>This is Landing</h2>
+    </div>
+  );
+};
+
+export default Landing;
