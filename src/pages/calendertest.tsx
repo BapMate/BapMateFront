fa496801ec2c351b0448d6b53a5fc2c9afd031fc
silver <ekdldkaa@naver.com>
2023-11-21 16:08:20 +0900

    [test]react-calender 날짜 클릭 시 띄우는 기능

--- src/pages/calendertest.tsx
+++ src/pages/calendertest.tsx
@@ -3,6 +3,7 @@ import Calendar from 'react-calendar';
 import { useEffect, useState } from 'react';
 import 'react-calendar/dist/Calendar.css';
 import moment from 'moment';
+import { formatDate } from 'react-calendar/dist/cjs/shared/dateFormatter';
 
 type ValuePiece = Date | null;
 
@@ -11,9 +12,17 @@ type Value = ValuePiece | [ValuePiece, ValuePiece];
 const CalenderTest = () => {
   const curDate = new Date(); // 현재 날짜
   const [value, onChange] = useState<Value>(curDate);
+  const [clickedDate, setClickedDate] = useState(''); //클릭한 데이터 string
 
   useEffect(() => {
-    console.log(value);
+    //클릭 시 날짜 띄우기
+    console.log(value); //Fri Nov 03 2023 00:00:00 GMT+0900 (Korean Standard Time)
+
+    //날짜 데이터 정제
+    const formattedDate = moment(String(value)).format('YYYY.MM.DD'); //2023.11.10
+    console.log(formattedDate);
+
+    setClickedDate(formattedDate);
   }, [value]);
 
   return (
@@ -25,7 +34,7 @@ const CalenderTest = () => {
         value={value}
         formatDay={(locale, date) => moment(date).format('DD')}
       />
-      {/* <div>{moment(value).format('YYYY년 MM월 DD일')}</div> */}
+      <div>{clickedDate}</div>
     </div>
   );
 };
