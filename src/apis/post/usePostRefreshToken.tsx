afa4f2850af00f6144460f80508fd59f44e226ad
silver <ekdldkaa@naver.com>
2023-11-23 16:17:17 +0900

    [feat]refreshToke api생성

---
+++ src/apis/post/usePostRefreshToken.tsx
@@ -0,0 +1,35 @@
+import { useMutation } from '@tanstack/react-query';
+import React from 'react';
+import axiosInstance from '..';
+
+/**
+ * refresh token으로 access token발급받기
+ * @param refresh token
+ * @returns access token
+ */
+export const usePostRefreshToken = (refreshToken: string | null) => {
+  const {
+    mutate,
+    data = '',
+    isPending,
+    error,
+    isSuccess,
+  } = useMutation({
+    mutationKey: ['refreshToken'],
+    mutationFn: async (data) => {
+      const res = await axiosInstance.post(
+        `/v1/auth/login?refresh=${refreshToken}`,
+        data,
+      );
+      return res.data;
+    },
+  });
+
+  return {
+    refreshToken: mutate,
+    isPending,
+    isSuccess,
+    error,
+    data,
+  };
+};
