2efb3c5337d0f6acf328bcefae601e0ba19fffdb
silver <ekdldkaa@naver.com>
2023-12-28 11:43:37 +0900

    fix: redirect url

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -6,7 +6,7 @@ import styled from 'styled-components';
 const Landing = () => {
   const moveToKakaoLogin = () => {
     window.location.href =
-      'https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=a0494a600b8fdb5783112c6329077f60&redirect_uri=http://localhost:3000/kakao/redirect';
+      'https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=a0494a600b8fdb5783112c6329077f60&redirect_uri=http://bap-mate-front.vercel.app/kakao/redirect';
   };
 
   return (
